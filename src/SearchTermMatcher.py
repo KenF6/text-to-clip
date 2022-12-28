fcdb88be43c4746e15c71070f928e614b6181091
KenF6 <KenF6@gmail.com>
2022-12-28 16:45:17 +0100

    Add functionality to find matches between the generated transcript and a given transcript

---
+++ src/SearchTermMatcher.py
@@ -0,0 +1,32 @@
+from difflib import SequenceMatcher
+from typing import List, Tuple
+
+
+class SearchTermMatcher:
+    def __init__(self):
+        pass
+
+    def find_sentence(self, sentence: str, text: str) -> List[Tuple[int, int, int]]:
+        indices = []
+        words = text.split()
+        sentence_words = sentence.split()
+
+        for sw_index, search_word in enumerate(sentence_words):
+            for i, word in enumerate(words):
+                if word == search_word:
+                    remaining_length = len(words) - i
+
+                    if remaining_length >= len(sentence_words):
+                        words_ = words[i - sw_index:i - sw_index + len(sentence_words)]
+                        s = SequenceMatcher(None, words_, sentence_words)
+
+                        if s.ratio() >= 0.4:
+                            indices.append((i - sw_index, i - sw_index + len(sentence_words), s.ratio()))
+
+        return self.deduplicate_tuples(indices)
+
+    def deduplicate_tuples(self, tuples: List[Tuple[int, int, int]]) -> List[Tuple[int, int, int]]:
+        s = set()
+        for t in tuples:
+            s.add(t)
+        return list(s)
