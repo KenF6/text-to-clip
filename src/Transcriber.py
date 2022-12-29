e8092fa30ea6a2b8814c65f813f5ad849429440f
KenF6 <KenF6@gmail.com>
2022-12-29 17:42:24 +0100

    Wire up Transcriber to automatically convert any media files to the format required by the transcription component

--- src/Transcriber.py
+++ src/Transcriber.py
@@ -1,5 +1,4 @@
 import json
-import sys
 from pathlib import PurePosixPath
 from typing import Any
 
@@ -11,42 +10,55 @@ from src.models.transcription.Transcript import Transcript
 
 class Transcriber:
     def __init__(self, input_filepath: PurePosixPath):
-        self.filepath: PurePosixPath = input_filepath
-        self.original_input_media_handler: MediaHandler = MediaHandler(self.filepath)
+        self.original_file: PurePosixPath = input_filepath
+        self.prepared_file: PurePosixPath = None
+        self.original_input_media_handler: MediaHandler = MediaHandler(self.original_file)
         self.prepared_input_media_handler: MediaHandler = None
 
-    def _create_prepared_filepath(self) -> PurePosixPath:
-        filename: PurePosixPath = self.filepath.stem
-        directory: PurePosixPath = self.filepath.parent
-        return directory / PurePosixPath(f"{filename}_prepared.wav")
-
     def create_transcript(self) -> Transcript:
-        if not self.is_format_supported():
-            print("Audio file must be WAV format mono PCM.")
-            sys.exit(1)
+        self._ensure_format_is_supported()
 
-        recognizer = self.initialize_recognizer(lang="en-us")
-        recognition_result = self.run_recognizer(recognizer)
+        recognizer = self._initialize_recognizer(lang="en-us")
+        recognition_result = self._run_recognizer(recognizer)
 
-        self.original_input_media_handler.close_wav_file()
+        self.prepared_input_media_handler.close_wav_file()
 
         return Transcript.from_dict(json.loads(recognition_result))
 
-    def is_format_supported(self) -> bool:
-        wave_file = self.original_input_media_handler.read_wav_file()
-        if wave_file.getnchannels() != 1 or wave_file.getsampwidth() != 2 or wave_file.getcomptype() != "NONE":
+    def _ensure_format_is_supported(self) -> None:
+        if self._is_format_supported():
+            self.prepared_file = self.original_file
+            self.prepared_input_media_handler = self.original_input_media_handler
+
+        else:
+            self.prepared_file = self._create_prepared_filepath()
+            self.original_input_media_handler.save_prepared_for_transcription(self.prepared_file)
+            self.prepared_input_media_handler = MediaHandler(self.prepared_file)
+
+    def _create_prepared_filepath(self) -> PurePosixPath:
+        filename: PurePosixPath = self.original_file.stem
+        directory: PurePosixPath = self.original_file.parent
+        return directory / PurePosixPath(f"{filename}_prepared.wav")
+
+    def _is_format_supported(self) -> bool:
+        media_info = self.original_input_media_handler.get_media_info()
+        if media_info.format.nb_streams != 1 \
+                or media_info.format.format_name != 'wav' \
+                or media_info.audio_streams[0].channels != 1 \
+                or media_info.audio_streams[0].codec_name != 'pcm_s16le':
             return False
         return True
 
-    def initialize_recognizer(self, lang: str) -> KaldiRecognizer:
+    def _initialize_recognizer(self, lang: str) -> KaldiRecognizer:
         model = Model(lang="en-us")
-        rec = KaldiRecognizer(model, self.original_input_media_handler.read_wav_file().getframerate())
+        rec = KaldiRecognizer(model, self.prepared_input_media_handler.read_wav_file().getframerate())
         rec.SetWords(True)
         return rec
 
-    def run_recognizer(self, recognizer: KaldiRecognizer) -> Any:
+    def _run_recognizer(self, recognizer: KaldiRecognizer) -> Any:
         while True:
-            data = self.original_input_media_handler.read_wav_file().readframes(self.original_input_media_handler.read_wav_file().getnframes())
+            data = self.prepared_input_media_handler.read_wav_file().readframes(
+                self.prepared_input_media_handler.read_wav_file().getnframes())
             if len(data) == 0:
                 break
             else:
@@ -54,4 +66,3 @@ class Transcriber:
 
         final_result = recognizer.FinalResult()
         return final_result
-
